68b35ae9ed9c64b29fc74bc61c6178e7229d0198
Mcebo <39724083+mxaba@users.noreply.github.com>
2024-02-06 19:26:21 +0200

    Server and DB connection setup done

--- index.ts
+++ index.ts
@@ -1,17 +1,37 @@
-import fastify, { FastifyInstance } from "fastify";
+'use strict';
 
-const app: FastifyInstance = fastify({logger: true});
+import fp from 'fastify-plugin';
+import { FastifyInstance } from 'fastify';
+import fastifyPostgres from '@fastify/postgres';
 
-app.get('/', (request, reply) => {
-    reply.send({hello: "world"})
-})
+const schema = {
+    type: 'object',
+    required: ['POSTGRES_URL', 'REDIS_URL', 'JWT_SECRET'],
+    properties: {
+        POSTGRES_URL: { type: 'string' },
+        REDIS_URL: { type: 'string' },
+        JWT_SECRET: { type: 'string' },
+    },
+    additionalProperties: false,
+};
+
+async function connectToDatabases(fastify: FastifyInstance) {
+    fastify.register(fastifyPostgres, {
+        connectionString: 'postgres://user:password@host:port/db',
+    });
+}
+
+async function authenticator(fastify: FastifyInstance) {
+    fastify.register(require('@fastify/jwt'), {
+        secret: "my-super-secret",
+        algorithms: ['RS256'],
+    });
+}
 
 
-app.listen(3000, (err, address) => {
-    if (err) {
-        app.log.error(err)
-        process.exit(1)
-    }
-    app.log.info(`Server listening on ${address}`)
-})
-
+export default async function (fastify: FastifyInstance, opts: any) {
+    fastify
+        .register(require('fastify-env'), { schema, data: [opts] })
+        .register(fp(connectToDatabases, opts))
+        .register(fp(authenticator))
+}
